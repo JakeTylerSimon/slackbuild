60027e938783ef90c14b7e5d30c665abd10a2f69
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-03 01:17:37 -0800

    update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABzQB3_tU2pmByvQCOY9F1Ws5j9_ZHmHU",
+    authDomain: "slackbuild-16d21.firebaseapp.com",
+    projectId: "slackbuild-16d21",
+    storageBucket: "slackbuild-16d21.appspot.com",
+    messagingSenderId: "403935010807",
+    appId: "1:403935010807:web:a3a8a2309d20961021d8f2"
+  };
