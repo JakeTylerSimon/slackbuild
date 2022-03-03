a8a7417f556b445b603d05e648191ef57f48e21d
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-03 02:33:47 -0800

    header update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Header from './components/Header';
+import styled from 'styled-components'
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets goooooo</h1>
+    
+      <Router>
+    
+        <Header/>
+
+        <AppBody>
+          <Sidebar/>
+          <Routes>
+            <Route path='/' exact />
+            {/* <Route path='/'  element={<Home/>} exact /> */}
+          </Routes>
+        </AppBody>
+
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+
+`
---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar;
+
+const SidebarContainer = styled.div`
+
+`
