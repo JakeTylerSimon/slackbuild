44a11491170485ec4cd196b61bbaa43e4d1652ce
Jake Tyler Simon <jtsimonbaseball@gmail.com>
2022-03-05 01:34:14 -0800

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyABzQB3_tU2pmByvQCOY9F1Ws5j9_ZHmHU",
     authDomain: "slackbuild-16d21.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "403935010807",
     appId: "1:403935010807:web:a3a8a2309d20961021d8f2"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider, db };
